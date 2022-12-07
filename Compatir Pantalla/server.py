95660710f5d65ec5d1a439040a2a93b192166236
Fabián <38739472+Imashi1@users.noreply.github.com>
2022-12-07 16:00:47 -0300

    a

--- Compatir Pantalla/server.py
+++ Compatir Pantalla/server.py
@@ -7,49 +7,28 @@ from mss import mss
 WIDTH = 1920
 HEIGHT = 1080
 
-<<<<<<< Updated upstream
-def retreive_screenshot(conn):
-=======
 
 
 def capturar_pantallazo(conn):
     #Captura de pantallazos continuos
->>>>>>> Stashed changes
     with mss() as sct:
         #Región a tomar capturas
         rect = {'top': 0, 'left': 0, 'width': WIDTH, 'height': HEIGHT}
 
         while 'recording':
-<<<<<<< Updated upstream
-            # carptura el screen
-            img = sct.grab(rect)
-            # Ajusta la  compresión (0-9)
-            pixels = compress(img.rgb, 6)
-
-            # Envia el tamañ la longitud de píxeles
-=======
             #Compresión de imagenes
             img = sct.grab(rect)
             pixels = compress(img.rgb, 6)
 
->>>>>>> Stashed changes
             size = len(pixels)
             size_len = (size.bit_length() + 7) // 8
             #Envía largo de pixeles
             conn.send(bytes([size_len]))
 
-<<<<<<< Updated upstream
-            # Envia la logintud actual
-            size_bytes = size.to_bytes(size_len, 'big')
-            conn.send(size_bytes)
-
-            # Envia los pixeles
-=======
             #Envía tamaño bytes
             size_bytes = size.to_bytes(size_len, 'big')
             conn.send(size_bytes)
 
->>>>>>> Stashed changes
             conn.sendall(pixels)
 
 def main():
